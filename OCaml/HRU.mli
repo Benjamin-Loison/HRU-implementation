cf3cd8664f52201f5c5d594c4adadf085df30b07
dsac <arnaud@dabyseesaram.info>
2022-01-04 13:04:04 +0100

    Pileup corrigé (commit oublié)

--- OCaml/HRU.mli
+++ OCaml/HRU.mli
@@ -3,26 +3,26 @@ val null_right : right_t
 type subject_t = string
 type object_t = string
 type state_r = {
-  subj : object_t list;
+  subj : subject_t list;
   obj : object_t list;
-  rights : (object_t * object_t, right_t) Hashtbl.t;
+  rights : (subject_t * object_t, right_t) Hashtbl.t;
 }
 type condition_t =
     Top
-  | Basic_condition of right_t * object_t * object_t
+  | Basic_condition of right_t * subject_t * object_t
   | And of condition_t * condition_t
   | Or of condition_t * condition_t
   | Neg of condition_t
 type operation_t =
-    Enter of right_t * object_t * object_t
-  | Delete of right_t * object_t * object_t
-  | Create_subject of object_t
-  | Destroy_subject of object_t
+    Enter of right_t * subject_t * object_t
+  | Delete of right_t * subject_t * object_t
+  | Create_subject of subject_t
+  | Destroy_subject of subject_t
   | Create_object of object_t
   | Destroy_object of object_t
 type command_t = condition_t * (operation_t list)
 val execute_operation : state_r -> operation_t -> state_r
-val checkRight : state_r -> right_t -> object_t -> object_t -> bool
+val checkRight : state_r -> right_t -> subject_t -> object_t -> bool
 val check_condition : state_r -> condition_t -> bool
 val test_equal : state_r -> state_r -> bool
 val execute_command : state_r -> command_t -> state_r
--- OCaml/main.ml
+++ OCaml/main.ml
@@ -1,29 +1,83 @@
 open HRU
 
+let pp (st: state_r) : unit =
+	Format.printf "\nSubjects:" ;
+	List.iter (fun s -> Format.printf "\n\t→ '%s'" s) st.subj;
+	Format.printf "\nObjects:" ;
+	List.iter (fun s -> Format.printf "\n\t→ '%s'" s) st.obj;
+	Format.printf "\nRights:" ;
+	Hashtbl.iter
+		(fun (s, o) r -> Format.printf "\n\t→ %d ∈ ('%s', '%s')" r s o) st.rights;
+	Format.printf "\n\n"
 
 
-let rec reach_failure (test: state_r -> bool)
-	(current_path: command_t list option)
-	(initial_state: state_r) (cmds: command_t list) : (command_t list) option =
-	if test initial_state
-		then current_path
-	else
-		List.fold_left
-			(fun cur_path cmd ->
-				match cur_path with
-				| Some p -> cur_path
-				| None ->
-				(
-					let new_state = execute_command initial_state cmd in
-					if test_equal new_state initial_state then
-						None
-					else
-						reach_failure test current_path new_state cmds
-				)
-			)
-			current_path
-			cmds
+
+let launch_pileup () =
+	(** Definition of subjects *)
+	let application : subject_t = "application"
+	and app_manager : subject_t = "application manager"
+	and perm_manager : subject_t = "permission manager"
+	(** Definition of objects *)
+	and permission : object_t = "Permission"
+	and application_obj : object_t = "Application (obj)"
+	(** Définition des droits *)
+	and has: right_t = 1 and exists : right_t = 2 and authorized : right_t = 4
+	and meaningful : right_t = 8 in
+	
+	(** État initial *)
+	let st_initial : state_r = {
+		subj = [application ; app_manager ; perm_manager] ;
+		obj = [permission ; application_obj] ;
+		rights = Hashtbl.create 1000
+	}
+
+	(** Définition des commandes *)
+	and install_1 : command_t =
+		(And(
+			Neg(Basic_condition(exists, app_manager, application_obj)),
+			Basic_condition(exists, perm_manager, permission)),
+		[Enter(authorized, application, permission) ;
+			Enter(has, application, permission) ;
+			Enter(exists, app_manager, application_obj)])
+	and install_0 : command_t =
+		(And(
+			Neg(Basic_condition(exists, app_manager, application_obj)),
+			Neg(Basic_condition(exists, perm_manager, permission))),
+		[Enter(has, application, permission) ;
+			Enter(exists, app_manager, application_obj) ;
+			Enter(exists, perm_manager, permission)])
+	and maj_0 : command_t =
+		(Neg(Basic_condition(exists, perm_manager, permission)),
+		[Enter(exists, perm_manager, permission) ;
+			Enter(meaningful, perm_manager, permission)])
+	and maj_1 : command_t =
+		(Basic_condition(exists, perm_manager, permission),
+		[Enter(meaningful, perm_manager, permission)]) in
+	
+	(** Définition de l'élévation de privilèges *)
+	let goal (st : state_r) : bool =
+		check_condition st
+			(And(
+				Basic_condition(exists, app_manager, application_obj),
+				And(
+					Basic_condition(exists, perm_manager, permission),
+					And(
+						Basic_condition(has, application, permission),
+						Basic_condition(meaningful, perm_manager, permission)))))
+		in
+	
+	(** Recherche d'un chemin *)
+	let path = path_finder st_initial
+		[install_0 ; install_1 ; maj_0 ; maj_1 ] goal 10 in
+	match path with
+	| None -> Format.printf "Blah blah blah\n"
+	| Some _ -> Format.printf "Hey, found one !\n"
+
 
 
 let _ =
-	Tests.test ()
+	(** Launching the tests 
+	Tests.test (); *)
+
+	(** Test to find a path for the Pileup problem *)
+	launch_pileup ()
